d6658446fa5b1d2777f4715477130642d411a82e
alexanderneva <amshulha@ualberta.ca>
2026-02-19 17:12:46 -0700

    done 2 adding 3

---
+++ hw3q2.R
@@ -0,0 +1,30 @@
+library(boot)
+set.seed(23)
+head(aircondit)
+# using the diff function to generate the differences
+times <- diff(aircondit$hours)
+print(times)
+# the MLE of of lambda is the sample mean
+lambda_hat <-  1 / mean(times)
+# bootstrap trials
+b <- 2000
+lambda_hat_star <- numeric(b)
+for (i in 1:b) {
+  x_star <- sample(times, length(times), replace = TRUE)
+  lambda_hat_star[i] <-  1 / mean(x_star)
+}
+cat("The bias is given by ", lambda_hat - mean(lambda_hat_star),
+    " the difference between the emprical and bootstrapped mean\n",
+    " and the standard error is the sampl standard deviation of",
+    " the bootstrap estimate", sd(lambda_hat_star), "\n")
+hist(lambda_hat_star, probability = TRUE,
+     main = "Bootstrap density vs Exp(lambda_hat)")
+# density curve
+y <- function(x) {
+  dexp(x, 1 / lambda_hat)
+}
+curve(y(x), col = 2, lwd = 2, add = TRUE)
+
+boot_se <- function(x) {
+  # function that takes in a bootstrap sample and outputs its standard error
+}
